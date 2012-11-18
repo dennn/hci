eeba8acda4b239ecaf26cb8a6599aa2f0cf7ad4b
Aiden Thompson <aiden.t@ntlworld.com>
2012-11-18 15:51:42 +0000

    Runs the entire test without sound.
    So far we can now run the same 7 tests, in a random order, at 4
    different 'levels' (sound to be implemented) and record the results.

--- experiment.js
+++ experiment.js
@@ -10,48 +10,109 @@ Parse.initialize("SSooDY5RjkTIeArUgMHRjw5NXExayT3c5jwvvqiy", "iyvaGqgdqmfldFuikg
 
 /* Paper Code */
 var canvasSize = view.size;
-var circleSize1 = 50;
-var circleSize2 = 75;
-var minDistance = 100; //the minimum distance two circles can be apart.
-var startTime = 0;
-var finishTime = 0;
-var ready = 0;
-var moving = 0;
-var done = 0;
-var missed = 0;
 var testSubjectNo = 0;
-var testNo = 1;
+var testNo = -1;
+var soundNo = 0;
 var timer1;
 var timer2;
+var position1;
+var position2;
+var c1id;
+var c2id;
 
-//Ask for the test subjects ID (to be filled in by tester)
+
+// "Constants" that define testing mode and tests:
+var totalSoundLevels = 4;
+var soundlevels = [0,40,60,100];
+var totalTestsPerLevel = 7;
+var tests = [[1234,125,631,168],[710,284,1060,345],[429,118,1237,229],[232,60,535,325],[479,135,941,377],[1320,400,483,329],[1398,149,367,297]];
+
+// Ask for the test subjects ID (to be filled in by tester)
 while ((testSubjectNo < 1) || (isNaN(testSubjectNo)))
   testSubjectNo = parseInt(prompt("Enter the test subjects ID:","1"));
   
 console.log("Test Subject:" + testSubjectNo);
 
-// Generate a bounding point so that part of the circle can not be off-screen:
-var bound1 = new Point(canvasSize.width - 2*circleSize1, canvasSize.height - 2.5*circleSize1)
-var bound2 = new Point(canvasSize.width - 2*circleSize2, canvasSize.height - 2.5*circleSize2)
+//Generate pre-testing information:
+// Shuffle the test order:
+var testOrder = [0,1,2,3,4,5,6];
+function randOrd(){
+  return (Math.round(Math.random())-0.5);
+}
+testOrder.sort(randOrd);
 
-// Generate the center of the circles:
-var position1 = Point.random() * bound1 + circleSize1;
-var position2 = Point.random() * bound2 + circleSize2;
-// If the circles are within the minimum allowed distance, regenerate the second circle:
-while (position1.isClose(position2, Math.max(circleSize1,circleSize2) + minDistance))
+var circleSize1 = 50;
+var circleSize2 = 75;
+var minDistance = 100; //the minimum distance two circles can be apart.
+var startTime;
+var finishTime;
+var ready;
+var moving;
+var done;
+var finished = 0;
+var missed;
+var circle1;
+var circle2;
+
+// Generate individual test information:
+function generateTest()
 {
-    position2 = Point.random() * bound2 + circleSize2;
+    // Init variables:
+    startTime = 0;
+    finishTime = 0;
+    ready = 0;
+    moving = 0;
+    done = 0;
+    missed = 0;
+
+    if (testNo < totalTestsPerLevel - 1) //next level
+        testNo++;
+    else //end of level
+    {
+       
+        if (soundNo < totalSoundLevels - 1)
+        {
+            testNo = 0;
+            soundNo++;
+            testOrder.sort(randOrd);
+        }
+        else //end of session
+        {
+            finished = 1;
+            console.log("End of testing.");
+            window.alert("All of your tests have finished.\nThank you for taking part.");
+            circle1.remove();
+            circle2.remove();
+            view.draw();
+            return;
+        }
+    }
+    
+    console.log("Test No: " + testNo);
+    
+    position1 = new Point(tests[testOrder[testNo]][0],tests[testOrder[testNo]][1]);
+    position2 = new Point(tests[testOrder[testNo]][2],tests[testOrder[testNo]][3]);
+
+    if (circle1)
+        circle1.remove();
+    if (circle2)
+        circle2.remove();
+    
+    // Create Paper.js Paths to draw a line into it:
+    circle1 = new Path.Circle(new Point(position1.x, position1.y), circleSize1);
+    circle1.fillColor = 'red';
+    circle1.smooth();
+    c1id = circle1.id;
+
+    circle2 = new Path.Circle(new Point(position2.x, position2.y), circleSize2);
+    circle2.fillColor = 'red';
+    circle2.smooth();
+    c2id = circle2.id;
+    circle2.visible = false;
 }
 
-// Create Paper.js Paths to draw a line into it:
-var circle1 = new Path.Circle(new Point(position1.x, position1.y), circleSize1);
-circle1.fillColor = 'red';
-circle1.smooth();
-
-var circle2 = new Path.Circle(new Point(position2.x, position2.y), circleSize2);
-circle2.fillColor = 'red';
-circle2.smooth();
-circle2.visible = false;
+//Run the first test generation:
+generateTest();
 
 //Detect when the mouse has hit the shape:
 var hitOptions = {
@@ -65,7 +126,8 @@ function saveResult() {
     var TestSubject = Parse.Object.extend("TestSubject");
     var testSubject = new TestSubject();
     testSubject.set("TestSubjectNo", testSubjectNo);
-    testSubject.set("TestNo", testNo);
+    testSubject.set("TestNo", testOrder[testNo]);
+    testSubject.set("soundLevel", soundlevels[soundNo]);
     testSubject.set("circleSize1", circleSize1);
     testSubject.set("circleSize2", circleSize2);
     testSubject.set("position1", position1);
@@ -86,75 +148,78 @@ function saveResult() {
 }
 
 function onMouseMove(event) {
-    var hitResult = project.hitTest(event.point, hitOptions);
-    
-    //When the circle1 item on the screen is hit and it hasn't been turned green:
-    if (hitResult && (hitResult.item.id == 2) && (circle1.fillColor.toString() != "{ red: 0, green: 1, blue: 0, alpha: 1 }"))
-    {
-        if (ready == 0)
+    if (!finished)
         {
-            hitResult.item.fillColor = 'yellow';
-        }
-        else
-        {
-            hitResult.item.fillColor = 'green';
-            circle2.visible = true;
-        }
-        if (startTime == 0)
-        {
-            //Put number of milliseconds since 1 Jan 1970 in startTime:
-            //(Note: this is the time that the user entered the circle, not nessersairly too useful for us but indicates
-            // that they have entered the circle nonetheless.)
-            startTime = new Date().getTime();
-            timer1 = setTimeout(function(){ready = 1;circle1.fillColor = 'green';circle2.visible = true;view.draw();},3000);
-        }
-    }
-    //When circle2 is hit and circle1 has turned green (timer1 has started)
-    else if (hitResult && (hitResult.item.id == 3) && moving)
-    {
-        finishTime = new Date().getTime();
-        circle2.fillColor = 'yellow';
-        view.draw();
-        timer2 = setTimeout(function(){circle2.fillColor = 'green';view.draw();done = 1;saveResult();console.log("Total Time: " + Math.abs(finishTime - startTime) + "ms");},1000);
-        moving = 0;
+        var hitResult = project.hitTest(event.point, hitOptions);
         
-    }
-    //When no items on the screen have been hit and the timer1 hasn't started:
-    else if (ready == 0)
-    {
-        //If the circle is yellow, they didn't stay there for long enough. Reset to red.
-        if (circle1.fillColor.toString() == "{ red: 1, green: 1, blue: 0, alpha: 1 }")
+        //When the circle1 item on the screen is hit and it hasn't been turned green:
+        if (hitResult && (hitResult.item.id == c1id) && (circle1.fillColor.toString() != "{ red: 0, green: 1, blue: 0, alpha: 1 }"))
         {
-            startTime = 0;
+            if (ready == 0)
+            {
+                hitResult.item.fillColor = 'yellow';
+            }
+            else
+            {
+                hitResult.item.fillColor = 'green';
+                circle2.visible = true;
+            }
+            if (startTime == 0)
+            {
+                //Put number of milliseconds since 1 Jan 1970 in startTime:
+                //(Note: this is the time that the user entered the circle, not nessersairly too useful for us but indicates
+                // that they have entered the circle nonetheless.)
+                startTime = new Date().getTime();
+                timer1 = setTimeout(function(){ready = 1;circle1.fillColor = 'green';circle2.visible = true;view.draw();},3000);
+            }
         }
-        if (startTime == 0)
-          circle1.fillColor = 'red';
-        else
+        //When circle2 is hit and circle1 has turned green (timer1 has started)
+        else if (hitResult && (hitResult.item.id == c2id) && moving)
         {
-          circle1.fillColor = 'green';
-          circle2.visible = true;
-          startTime = 0;
+            finishTime = new Date().getTime();
+            circle2.fillColor = 'yellow';
+            view.draw();
+            timer2 = setTimeout(function(){circle2.fillColor = 'green';done = 1;saveResult();console.log("Total Time: " + Math.abs(finishTime - startTime) + "ms");generateTest();view.draw();},1000);
+            moving = 0;
+            
         }
-        clearTimeout(timer1);
-        timer1 = null;
-    }
-    //On first leaving the green and ready to go circle:
-    else if (!moving && finishTime == 0)
-    {
-        startTime = new Date().getTime();
-        console.log("Started Moving at: " + startTime);
-        moving = 1;
-    }
-    //Entered circle and left again before 1 second verification:
-    else if (done == 0 && finishTime != 0 && !hitResult)
-    {
-        clearTimeout(timer2);
-        timer2 = null;
-        finishTime = 0;
-        missed = 1;
-        moving = 1;
-        circle2.fillColor = 'red';
-        view.draw();
+        //When no items on the screen have been hit and the timer1 hasn't started:
+        else if (ready == 0)
+        {
+            //If the circle is yellow, they didn't stay there for long enough. Reset to red.
+            if (circle1.fillColor.toString() == "{ red: 1, green: 1, blue: 0, alpha: 1 }")
+            {
+                startTime = 0;
+            }
+            if (startTime == 0)
+              circle1.fillColor = 'red';
+            else
+            {
+              circle1.fillColor = 'green';
+              circle2.visible = true;
+              startTime = 0;
+            }
+            clearTimeout(timer1);
+            timer1 = null;
+        }
+        //On first leaving the green and ready to go circle:
+        else if (!moving && finishTime == 0)
+        {
+            startTime = new Date().getTime();
+            console.log("Started Moving at: " + startTime);
+            moving = 1;
+        }
+        //Entered circle and left again before 1 second verification:
+        else if (done == 0 && finishTime != 0 && !hitResult)
+        {
+            clearTimeout(timer2);
+            timer2 = null;
+            finishTime = 0;
+            missed = 1;
+            moving = 1;
+            circle2.fillColor = 'red';
+            view.draw();
+        }
     }
 }
 
