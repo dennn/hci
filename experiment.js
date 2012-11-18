be9f9bb5718366add3bee387dfbf0f54a48dc9a5
Aiden Thompson <aiden.t@ntlworld.com>
2012-11-18 13:23:07 +0000

    Update
    A basic system now works with 1 test and uploads the result to parse.

--- experiment.js
+++ experiment.js
@@ -1,9 +1,9 @@
-/* JS Setup code */
-window.onload = function() {
+/* JS Setup code - Moved back to html otherwise it doesn't work!*/
+/*window.onload = function() {
      var canvas = document.getElementById("experiment");
      canvas.height = document.documentElement.clientHeight;
      canvas.width  = document.documentElement.clientWidth;
-}
+}*/
 
 /* Parse Setup Code */
 Parse.initialize("SSooDY5RjkTIeArUgMHRjw5NXExayT3c5jwvvqiy", "iyvaGqgdqmfldFuikgxgW6wVWPCxFD7yopIk2fGn");
@@ -11,22 +11,37 @@ Parse.initialize("SSooDY5RjkTIeArUgMHRjw5NXExayT3c5jwvvqiy", "iyvaGqgdqmfldFuikg
 /* Paper Code */
 var canvasSize = view.size;
 var circleSize1 = 50;
+var circleSize2 = 75;
+var minDistance = 100; //the minimum distance two circles can be apart.
 var startTime = 0;
+var finishTime = 0;
 var ready = 0;
 var moving = 0;
+var testSubjectNo = 1;
+var testNo = 1;
 var timer;
 
 // Generate a bounding point so that part of the circle can not be off-screen:
-var bound = new Point(canvasSize.width - 2*circleSize1, canvasSize.height - 2.5*circleSize1)
+var bound1 = new Point(canvasSize.width - 2*circleSize1, canvasSize.height - 2.5*circleSize1)
+var bound2 = new Point(canvasSize.width - 2*circleSize2, canvasSize.height - 2.5*circleSize2)
 
-// Generate the center of the circle:
-var position = Point.random() * bound + circleSize1;
+// Generate the center of the circles:
+var position1 = Point.random() * bound1 + circleSize1;
+var position2 = Point.random() * bound2 + circleSize2;
+// If the circles are within the minimum allowed distance, regenerate the second circle:
+while (position1.isClose(position2, Math.max(circleSize1,circleSize2) + minDistance))
+{
+    position2 = Point.random() * bound2 + circleSize2;
+}
 
-// Create a Paper.js Path to draw a line into it:
-var circle1 = new Path.Circle(new Point(position.x, position.y), circleSize1);
-// Give the stroke a color:
+// Create Paper.js Paths to draw a line into it:
+var circle1 = new Path.Circle(new Point(position1.x, position1.y), circleSize1);
 circle1.fillColor = 'red';
 circle1.smooth();
+
+var circle2 = new Path.Circle(new Point(position2.x, position2.y), circleSize2);
+circle2.fillColor = 'red';
+circle2.smooth();
 
 //Detect when the mouse has hit the shape:
 var hitOptions = {
@@ -35,6 +50,29 @@ var hitOptions = {
     fill: true,
     tolerance: 0 //to hitting the shape
 };
+
+function saveResult() {
+    var TestSubject = Parse.Object.extend("TestSubject");
+    var testSubject = new TestSubject();
+    testSubject.set("TestSubjectNo", testSubjectNo);
+    testSubject.set("TestNo", testNo);
+    testSubject.set("circleSize1", circleSize1);
+    testSubject.set("circleSize2", circleSize2);
+    testSubject.set("position1", position1);
+    testSubject.set("position2", position2);
+    testSubject.set("minDistance", minDistance);
+    testSubject.set("circleDistance", position1.getDistance(position2, 0));
+    testSubject.set("totalTime", Math.abs(finishTime - startTime));    
+    
+    testSubject.save(null, {
+    success: function(testSubject) {
+      console.log("Saved successfully!");
+    },
+    error: function(testSubject, error) {
+      window.alert("Save failed!");
+    }
+  });
+}
 
 function onMouseMove(event) {
     var hitResult = project.hitTest(event.point, hitOptions);
@@ -56,11 +94,18 @@ function onMouseMove(event) {
             //(Note: this is the time that the user entered the circle, not nessersairly too useful for us but indicates
             // that they have entered the circle nonetheless.)
             startTime = new Date().getTime();
-            console.log(startTime);
             timer = setTimeout(function(){ready = 1;circle1.fillColor = 'green';view.draw();},3000);
         }
     }
-    //When no items on the screen have been hit:
+    //When circle2 is hit and circle1 has turned green (timer has started)
+    else if (hitResult && (hitResult.item.id == 3) && moving)
+    {
+        finishTime = new Date().getTime();
+        console.log("Total Time: " + Math.abs(finishTime - startTime) + "ms");
+        moving = 0;
+        saveResult();
+    }
+    //When no items on the screen have been hit and the timer hasn't started:
     else if (ready == 0)
     {
         //If the circle is yellow, they didn't stay there for long enough. Reset to red.
@@ -79,7 +124,7 @@ function onMouseMove(event) {
         timer = null;
     }
     //On first leaving the green and ready to go circle:
-    else if (!moving)
+    else if (!moving && finishTime == 0)
     {
         startTime = new Date().getTime();
         console.log("Started Moving at: " + startTime);
